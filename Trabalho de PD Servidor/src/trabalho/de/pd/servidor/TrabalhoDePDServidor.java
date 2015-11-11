828a24bf28595b1832a8f7f4565c851fc963c73f
ASUS <ASUS@10.65.138.223>
2015-11-11 16:54:44 +0000

    A seguir e a por a enviar objetos

--- Trabalho de PD Servidor/src/trabalho/de/pd/servidor/TrabalhoDePDServidor.java
+++ Trabalho de PD Servidor/src/trabalho/de/pd/servidor/TrabalhoDePDServidor.java
@@ -22,7 +22,7 @@ public class TrabalhoDePDServidor {
     private DatagramSocket socket;
     private DatagramPacket packet;
     String receiveMSG,sendMSG;
-    private boolean debug;
+    private boolean debug,Primario;
     /**
      * @param args the command line arguments
      */
@@ -30,10 +30,20 @@ public class TrabalhoDePDServidor {
         // TODO code application logic here
         socket=null;
         packet=new DatagramPacket(new byte[MAX_SIZE],MAX_SIZE);
-        socket=new DatagramSocket(7000);       
-        run();
-        
-        
+        socket=new DatagramSocket(7000);
+        Primario=false;
+        this.debug=debug;
+        try{
+            run();
+        }catch(NumberFormatException e){
+            System.out.println("O porto de escuta deve ser um inteiro positivo.");
+        }catch(SocketException e){
+            System.out.println("Ocorreu um erro ao nível do socket UDP:\n\t"+e);
+        }catch(IOException e){
+            System.out.println("Ocorreu um erro no acesso ao socket:\n\t"+e);
+        }finally{
+                closeSocket();
+        }
     }
     
     public void run() throws IOException
@@ -41,8 +51,7 @@ public class TrabalhoDePDServidor {
         if(socket==null)
         {
             return;
-        }
-        
+        }       
         if(debug)
         {
             System.out.println("UDP do server iniciado...");
@@ -50,15 +59,44 @@ public class TrabalhoDePDServidor {
         
         while(true)
         {
-            
+            receiveMSG=receive();    
+            if(receiveMSG==null)
+            {
+                continue;
+            }
+            send();
         }
     }
     
-    public String receive() throws IOException
+    public String receive() throws IOException                
     {
         String recv;
         
+        if(socket==null)
+        {
+            return null;
+        }
         
+        socket.receive(packet);
+        recv=new String(packet.getData(),0,packet.getLength());
+        if(debug)
+        {
+            System.out.println("Recevido:"+recv+" do IP:"+packet.getAddress().getHostAddress()+" e de Porto:"+packet.getPort());
+        }
+        return recv;
     }
     
+    public void send() throws IOException
+    {
+        sendMSG=("Enviado do servidor");
+        packet.setData(sendMSG.getBytes());
+        socket.send(packet);
+    }
+    
+    
+    public void closeSocket()
+    {
+        if(socket!=null)
+            socket.close();
+    }
 }
