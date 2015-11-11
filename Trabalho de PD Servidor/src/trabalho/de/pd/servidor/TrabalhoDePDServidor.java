434cc14472deff53a33b78b73249d0176f6970e5
ASUS <ASUS@10.65.138.223>
2015-11-11 16:06:13 +0000

    fiz servidor

--- Trabalho de PD Servidor/src/trabalho/de/pd/servidor/TrabalhoDePDServidor.java
+++ Trabalho de PD Servidor/src/trabalho/de/pd/servidor/TrabalhoDePDServidor.java
@@ -5,17 +5,60 @@
  */
 package trabalho.de.pd.servidor;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 /**
  *
  * @author Rafael
  */
-public class TrabalhoDePDServidor {
 
+public class TrabalhoDePDServidor {
+    public static final int MAX_SIZE=256;
+    public static final String TIME_REQUEST="";
+    
+    private DatagramSocket socket;
+    private DatagramPacket packet;
+    String receiveMSG,sendMSG;
+    private boolean debug;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public void main(String[] args) throws SocketException{
         // TODO code application logic here
+        socket=null;
+        packet=new DatagramPacket(new byte[MAX_SIZE],MAX_SIZE);
+        socket=new DatagramSocket(7000);       
+        run();
+        
+        
+    }
+    
+    public void run() throws IOException
+    {     
+        if(socket==null)
+        {
+            return;
+        }
+        
+        if(debug)
+        {
+            System.out.println("UDP do server iniciado...");
+        }
+        
+        while(true)
+        {
+            
+        }
+    }
+    
+    public String receive() throws IOException
+    {
+        String recv;
+        
+        
     }
     
 }
