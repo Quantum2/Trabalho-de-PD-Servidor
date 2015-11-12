429a305594337e819b3289843f5bb25325ac4885
ASUS <ASUS@192.168.121.145>
2015-11-12 10:56:55 +0000

    heartbeats quase feitos, acho eu

--- Trabalho de PD Servidor/src/trabalho/de/pd/servidor/Servidor.java
+++ Trabalho de PD Servidor/src/trabalho/de/pd/servidor/Servidor.java
@@ -15,8 +15,11 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.ServerSocket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -24,28 +27,34 @@ import java.net.UnknownHostException;
  */
 public class Servidor implements Serializable{
     public static final int MAX_SIZE=1000;
-    public static final String TIME_REQUEST="";
     public InetAddress group;
     public int port;
+    public int contador=0;
     
+    //threads
     public Runnable HeartbeatsEnvia=null;
     public Runnable HeartbeatsRecebe=null;
     
-    protected MulticastSocket socket=null;
-    private DatagramPacket packet=null;
+    //UDP
+    protected MulticastSocket socketUDP=null;
+    private DatagramPacket packetUDP=null;
+    
+    //TCP
+    private ServerSocket socketTCP=null;
+    
     Object receiveMSG,sendMSG;
-    private boolean debug,Primario;
+    private boolean debug,Primario,Linked;
     
     public Servidor(String diretoria,int Porto) throws UnknownHostException, IOException, InterruptedException 
     {       
         group=InetAddress.getByName("225.15.15.15");
-        packet=new DatagramPacket(new byte[MAX_SIZE],MAX_SIZE);
+        packetUDP=new DatagramPacket(new byte[MAX_SIZE],MAX_SIZE);
         port=Porto;
-        socket = new MulticastSocket(port);
-        socket.joinGroup(group);
+        socketUDP = new MulticastSocket(port);
+        socketUDP.joinGroup(group);
         Primario=false;
+        Linked=false;
         debug=true;
-        
         
         try{
             constroiThreads();
@@ -71,13 +80,16 @@ public class Servidor implements Serializable{
             public void run() {
                 do {
                     try {
-                        ByteArrayOutputStream byteout = new ByteArrayOutputStream(MAX_SIZE);
-                        ObjectOutputStream send = new ObjectOutputStream(byteout);
-                        send.writeObject(Primario);
-                        packet.setData(byteout.toByteArray());
-                        packet.setLength(byteout.size());
-                        socket.send(packet);
-                        Thread.sleep(5000);
+                        if(Linked==true || Primario==true)
+                        {
+                            ByteArrayOutputStream byteout = new ByteArrayOutputStream(MAX_SIZE);
+                            ObjectOutputStream send = new ObjectOutputStream(byteout);
+                            send.writeObject(Primario);
+                            packetUDP.setData(byteout.toByteArray());
+                            packetUDP.setLength(byteout.size());
+                            socketUDP.send(packetUDP);
+                            Thread.sleep(5000);
+                        }
                     } catch (NumberFormatException e) {
                         System.out.println("O porto de escuta deve ser um inteiro positivo.");
                     } catch (SocketException e) {
@@ -87,8 +99,8 @@ public class Servidor implements Serializable{
                     } catch (InterruptedException e) {
                         System.out.println("Ocorreu um erro no sleep");
                     } finally {
-                        closeSocket();
-                    }
+                        
+                    }                   
                 } while (debug);
             }
         };
@@ -97,9 +109,45 @@ public class Servidor implements Serializable{
 
             @Override
             public void run() {
-                
-            }
-            
+                do{                  
+                    try {
+                        if (Linked == false && Primario == false) {
+                            if (contador == 3) {
+                                Primario = true;
+                            } else {
+                                ObjectInputStream recv = null;
+                                socketUDP.receive(packetUDP);
+                                recv = new ObjectInputStream(new ByteArrayInputStream(packetUDP.getData()));
+                                Object msg = recv.readObject();
+                                if (msg instanceof Boolean) {
+                                    if (true == (Boolean) msg) {
+                                        Linked = true;
+                                        socketTCP = new ServerSocket(socketUDP.getPort(), 0, socketUDP.getInetAddress());
+                                        //falta receber todos os dados do servidor primario
+                                    }
+                                }
+                                Thread.sleep(5000);
+                            }
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("O porto de escuta deve ser um inteiro positivo.");
+                    } catch (SocketException e) {
+                        System.out.println("Ocorreu um erro ao nível do socket UDP:\n\t" + e);
+                    } catch (IOException e) {
+                        System.out.println("Ocorreu um erro no acesso ao socket:\n\t" + e);
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+                    } finally {
+                        if(Linked==false && Primario==false)
+                            contador++;
+                        else{
+                            contador=0;
+                        }
+                    }
+                }while(Linked);
+            }         
         };
         
     }
@@ -107,7 +155,7 @@ public class Servidor implements Serializable{
     public void começa() throws IOException,InterruptedException
     {     
         debug=true;
-        if(socket==null)
+        if(socketUDP==null)
         {
             return;
         }       
@@ -129,31 +177,9 @@ public class Servidor implements Serializable{
         }
     }
     
-    public Object receive() throws IOException                
-    {
-        ObjectInputStream recv;
-        
-        if(socket==null)
-        {
-            return null;
-        }
-        
-        socket.receive(packet);
-        recv=new ObjectInputStream(new ByteArrayInputStream(packet.getData()));                       //ClassCastException | ClassNotFoundException 
-        
-        if(debug)
-        {
-            System.out.println("Recevido do IP:"+packet.getAddress().getHostAddress()+" e de Porto:"+packet.getPort());
-        }
-        return recv;
-    }
-    
-    
-    
-    
     public void closeSocket()
     {
-        if(socket!=null)
-            socket.close();
+        if(socketUDP!=null)
+            socketUDP.close();
     }
 }
